0fd9b9e19558c2d1ac28a9dd34c8ca22ebd52eef
Fausto Lopez <152632614+Gileserix@users.noreply.github.com>
2025-06-04 22:49:48 +0200

    Update App.js

--- foot-trading/src/App.js
+++ foot-trading/src/App.js
@@ -9,12 +9,11 @@ import CreaTuProducto from './pages/createYourProduct.jsx';
 import Accesorios from './pages/accessories.jsx';
 import Register from './pages/register.jsx';
 import Login from './components/login.jsx'; // Importa el componente de inicio de sesión
-import EmailConfirmation from './pages/EmailConfirmation.jsx';
 import Profile from './pages/profile.jsx'; // Importa el componente de perfil
+import MyProducts from './pages/myProducts';
 import { CartProvider } from './services/cartContext.js';
 import { ProductProvider } from './services/productContext.js';
-import { UserProvider } from './services/userContext.js';
-
+import { UserProvider } from './services/userContext.js'; // Importa el UserProvider
 
 function App() {
   return (
@@ -31,9 +30,9 @@ function App() {
                 <Route path="/create-your-product" element={<CreaTuProducto />} />
                 <Route path="/accessories" element={<Accesorios />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/verify-email" element={<EmailConfirmation />} /> {/* Añade la ruta de confirmación de correo electrónico */}
                 <Route path="/login" element={<Login />} /> {/* Añade la ruta de inicio de sesión */}
                 <Route path="/profile" element={<Profile />} /> {/* Añade la ruta de perfil */}
+                <Route path="/my-products" element={<MyProducts />} /> {/* Nueva ruta */}
                 {/* Add other routes here */}
               </Routes>
               <Footer />
